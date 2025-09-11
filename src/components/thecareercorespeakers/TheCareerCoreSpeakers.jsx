cc783d68a531edac026cb00720085f607f5f3dfd
mfonmfon <mfonm579@gmail.com>
2025-09-11 01:17:04 +0100

    updated

--- src/components/thecareercorespeakers/TheCareerCoreSpeakers.jsx
+++ src/components/thecareercorespeakers/TheCareerCoreSpeakers.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import UnknownSpeakerImage from '../../assets/images/UnknownFaceImage.jpg'
 import AdewumiAphonosoImage from '../../assets/images/panel-speakers/Adewumi Alphonso.jpeg'
 import OmolaraDada from '../../assets/images/panel-speakers/OMOLARA DADA.jpg'
+import EmmanuelFaithImage from '../../assets/images/speakers/Emmanuel Faith.jpg'
+import KemaOyihImage from '../../assets/images/speakers/Kema Oyih.JPG'
+import OyindaAkinyemiImage from '../../assets/images/speakers/Oyinda Akinyemi.jpg'
+import TemitopeOladokunImage from '../../assets/images/speakers/Temitope Oladokun Headshot.jpeg'
 
 const speakersData = [
   {
@@ -15,24 +19,24 @@ const speakersData = [
     speakersImage: OmolaraDada,
   },
   {
-    name: 'Coming soon',
-    // jobTitle: 'CTO, Future Tech',
-    speakersImage: UnknownSpeakerImage,
+    name: 'Emmanuel Faith',
+    jobTitle: 'HR professional, consultant, and capacity builder',
+    speakersImage: EmmanuelFaithImage,
   },
   {
-    name: 'Coming sooon',
-    // jobTitle: 'Head of Marketing, Global Brands',
-    speakersImage: UnknownSpeakerImage,
+    name: 'Kema Oyih',
+    jobTitle: 'Artist manager and operations specialist',
+    speakersImage: KemaOyihImage,
   },
   {
-    name: 'Coming soon',
-    // jobTitle: 'Head of Marketing, Global Brands',
-    speakersImage: UnknownSpeakerImage,
+    name: 'Oyinda Akinyemi',
+    jobTitle: 'Heads up Equity Capital Markets for Africa Regions in the Standard Bank Group',
+    speakersImage: OyindaAkinyemiImage,
   },
   {
-    name: 'Coming soon',
-    // jobTitle: 'Head of Marketing, Global Brands',
-    speakersImage: UnknownSpeakerImage,
+    name: 'Temitope Oladokun',
+    jobTitle: 'Director of Customer Management at Coca-Cola',
+    speakersImage: TemitopeOladokunImage,
   },
   {
     name: 'Coming soon',
@@ -40,7 +44,6 @@ const speakersData = [
     speakersImage: UnknownSpeakerImage,
   },
 ]
-
 const TheCareerCoreSpeakers = () => {
   return (
     <div className='w-full min-h-screen bg-gradient-to-br from-blue-900 via-black to-blue-900 px-4 md:px-8 lg:px-16 py-16 overflow-hidden'>
